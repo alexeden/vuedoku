eecd7af44717099a81745f902c58e43de9a0cf76
Alexander Eden <alexandereden91@gmail.com>
2017-10-09 22:39:52 -0500

    Divide up by nonets instead of rows

--- src/components/board.component.ts
+++ src/components/board.component.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import { mapState } from 'vuex';
 import { State } from 'sudoku/store';
+import { Cell } from 'sudoku/lib';
 import { CellComponent } from './cell.component';
 
 interface BoardComponent extends Vue {
   chunkify<T>(size: number, list: T[]): T[][];
+  cells: Cell[];
 }
 
 export const BoardComponent: Vue.ComponentOptions<BoardComponent> = {
@@ -14,18 +16,41 @@ export const BoardComponent: Vue.ComponentOptions<BoardComponent> = {
 
   template: `
     <div class="sudoku-board">
-      <div v-for="row in chunkify(9, board)" class="sudoku-board__row">
-        <div class="sudoku-board__cell-wrapper" v-for="cell in row">
+      <div
+        v-for="(nonet, nonetIndex) in nonets"
+        :key="nonetIndex"
+        :class="'sudoku-board__nonet--' + nonetIndex"
+        class="sudoku-board__nonet">
+        <div
+          v-for="cell in nonet"
+          :key="cell.index"
+          class="sudoku-board__cell-wrapper">
           <cell :cell="cell" v-bind="cell"/>
         </div>
       </div>
+
     </div>
   `,
   computed: {
     ...mapState({
-      board: (state: State) => state.board.cells,
+      cells: (state: State) => state.board.cells,
       cursor: (state: State) => state.board.cursor
-    })
+    }),
+    nonets(): Cell[][] {
+      const indexedNonets: {[nonet: number]: Cell[]}
+        = this.cells
+            .reduce(
+              (obj, cell) => ({
+                ...obj,
+                [cell.nonet]: (obj[cell.nonet] || []).concat(cell)
+              }),
+              {}
+            );
+
+      return Object.keys(indexedNonets)
+        .sort()
+        .map(index => indexedNonets[index]);
+    }
   },
   methods: {
     chunkify<T>(this: BoardComponent, size: number, list: T[]): T[][] {
