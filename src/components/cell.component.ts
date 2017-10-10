0a497be0c8a2907688bd0c735fb361072b1180a9
Alexander Eden <alexandereden91@gmail.com>
2017-10-10 00:54:38 -0500

    Chose a font

--- src/components/cell.component.ts
+++ src/components/cell.component.ts
@@ -46,8 +46,8 @@ export const CellComponent: Vue.ComponentOptions<any> = {
         'sudoku-cell--col-selected': this.colSelected,
         'sudoku-cell--nonet-selected': this.nonetSelected,
         'sudoku-cell--selected': this.selected,
-        'z-depth-3': this.selected,
-        'z-depth-1': !this.selected && (this.rowSelected || this.colSelected || this.nonetSelected),
+        // 'z-depth-3': this.selected,
+        // 'z-depth-1': !this.selected && (this.rowSelected || this.colSelected || this.nonetSelected),
         'sudoku-cell--impossible': this.hasImpossibleValue,
         'sudoku-cell--matches-selected': this.$store.getters.selectedCell.value === this.value
       };
