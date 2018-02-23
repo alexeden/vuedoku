5687b1016fb99d748aa7a16da8ef9ae85d782627
Alex Eden <alexandereden91@gmail.com>
2018-02-22 18:41:09 -0600

    Add a (so far unused) types file

---
+++ src/store/types.ts
@@ -0,0 +1,14 @@
+export const types = {
+  game: {
+    /* Getters */
+    selectedCell: 'types/game/selectedCell',
+    selectedCellValue: 'types/game/selectedCellValue',
+    impossibleValues: 'types/game/impossibleValues',
+    hasImpossibleValue: 'types/game/hasImpossibleValue',
+    valueIsComplete: 'types/game/valueIsComplete',
+    possibleCellValues: 'types/game/possibleCellValues',
+    totalRemainingValues: 'types/game/totalRemainingValues',
+    remainingValueCounts: 'types/game/remainingValueCounts',
+    remaingValueOf: 'types/game/remaingValueOf'
+  }
+};
